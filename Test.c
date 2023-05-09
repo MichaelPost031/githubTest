491e8a48406a07cd2b9e02b422dca399cff9bcf1
MichaelPost031 <michael.a.post031@gmail.com>
2023-05-08 21:39:47 -0500

    Testing add to array

--- Test.c
+++ Test.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    printf("%s", "Hello World!");
+int addToArray(int array[], int intToAdd){
+    int truthValue = 0;
+    for(int i = 0; i < 10; i++){
+        if(array[i] == intToAdd){
+            truthValue == 1;
+        }  
+    }
+    for(int i = 0; i < 10; i++){
+        if(array[i] == NULL){
+            array[i] == intToAdd;
+        }  
+    }
 }
+
+int main(){
+    int array[10];
+    array[3] = 5;
+    printf("%s\n", "Hello World!");
+    addToArray(array, 5);
+    for(int i = 0; i < 10; i++){
+        printf("%d\n", array[i]);
+    }
+}
+
